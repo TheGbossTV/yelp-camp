c5a6d83f1c516c0422d90bdd18dd9bcfe6b2abc3
GuilhermeSSilva <guilhermessilva26@gmail.com>
2025-06-06 22:44:20 +0100

    Added types

--- src/Pages/HomePage/index.tsx
+++ src/Pages/HomePage/index.tsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
+import { type UserInfo } from "../../types/types";
 
 const HomePage = () => {
-  const [userInfo, setUserInfo] = useState<any>(null);
+  const [userInfo, setUserInfo] = useState<UserInfo | null>(null);
 
   const getUserInfo = async () => {
     const response = await fetch("http://localhost:3000/userinfo");
     const data = await response.json();
-    console.log(data);
     setUserInfo(data);
   };
 
---
+++ src/types/types.ts
@@ -0,0 +1,9 @@
+export type UserInfo = {
+  user: {
+    email: string;
+    id: string;
+    user_metadata: {
+      username: string;
+    };
+  };
+};
