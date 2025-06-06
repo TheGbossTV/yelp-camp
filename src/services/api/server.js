5eafe4aabcb43282054aafc0fac719f6f15b6c63
GuilhermeSSilva <guilhermessilva26@gmail.com>
2025-06-06 14:24:39 +0100

    Idk

--- src/services/api/server.js
+++ src/services/api/server.js
@@ -1,8 +1,9 @@
-import express from 'express'
+import express from "express";
+// import { supabase } from "../supabase-config-client";
 
-const app = express()
-const PORT = 3000
+const app = express();
+const PORT = 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
